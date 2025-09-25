03b23c331a4c7d0fbb18c4179d800cfecc926bde
BlackB34rd <contact@the-tiny-spark.com>
2025-09-25 11:26:20 +0200

    basics types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-function hello(){
-    console.log("Hello, world and coucou!");
-}
+// Array of strings
+const tab:string[] = ["HTML", "CSS", "JS", "TS"];
+console.log(tab);
 
-hello();
+const isFormateurSuperCool: boolean = true;
+console.log(isFormateurSuperCool);
+
+// null && undefined
+
+let toto = null;
+let tata;
+console.log(toto, tata, tata === toto);
+
+
+// number and strings 
+let value1 = 40; 
+let value2 = [40];
+
+
+// cast string to number automatically
+console.log(value1 == value2);
+
+// with === there's no automatic cast
+console.log(value1 === value2);
+
+// eval
+const obj = eval("{name: 'toto'}");
+console.log(obj);
