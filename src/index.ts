c65b7d5dcec0b5e3980acef747dfeb129df30c05
BlackB34rd <contact@the-tiny-spark.com>
2025-09-25 11:43:56 +0200

    add tuples

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-enum Direction {
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3
-}; 
-
-const num = 3;
-const dir:Direction = num;
-
-let haut:Direction = Direction.Up;
-let bas:Direction = Direction.Down;
-let left:Direction = Direction.Left;
-let right:Direction = Direction.Right;
-
-console.log(dir === Direction.Right, haut, bas, left, right);
+let utilisateurs:[string, number] = ["Alice", 30];
+console.log(utilisateurs);
