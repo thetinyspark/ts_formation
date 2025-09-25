fd2c689818aeada58a23c9ed2e60c875ac3e642f
BlackB34rd <contact@the-tiny-spark.com>
2025-09-25 11:51:02 +0200

    add void and never type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
+function helloWorld():void{
+    console.log("Hello, world!");
+}
 
-let utilisateurs:Map<string,number> = new Map<string,number>();
-utilisateurs.set("Alice", 30);
-utilisateurs.set("Bob", 25);
-
-console.log(utilisateurs.keys());
-console.log(utilisateurs.values());
+helloWorld();
