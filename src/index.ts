a90bb991f4dda20ab8d056c3af324cbf46fd913a
BlackB34rd <contact@the-tiny-spark.com>
2025-09-25 12:20:32 +0200

    add classes, heritage and TP n°1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-function helloWorld():void{
-    console.log("Hello, world!");
+// classe
+class Heroe{
+    public name:string;
+    protected _age:number;
+
+    constructor(name:string){
+        this.name = name;
+        this._age = 30;
+    }
+
+    public introduceMySelf():void{
+        console.log(this.name +","+this._age);
+    }
 }
 
-helloWorld();
+
+// héritage
+class SuperHeroe extends Heroe{
+    constructor(name:string){
+        // appaler au constructeur de la classe parente
+        super(name);
+        this._age = 41;
+    }
+
+    // surcharge de méthode
+    public introduceMySelf():void{
+        super.introduceMySelf(); // appel de la méthode parente
+        console.log("Je suis " + this.name + ", " + this._age + " ans");
+    }
+}
+
+const heroe = new Heroe("Batman");
+const heroe2 = new SuperHeroe("Spiderman");
+heroe.introduceMySelf();
+heroe2.introduceMySelf();
