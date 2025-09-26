6b84321127e9bf3449ce9cc150508bf28d6059e7
BlackB34rd <contact@the-tiny-spark.com>
2025-09-26 11:09:27 +0200

    add override on isAlive method on ShonenHeroe

--- src/heroes/ShonenHeroe.ts
+++ src/heroes/ShonenHeroe.ts
@@ -5,4 +5,14 @@ export class ShonenHeroe extends Heroe implements IFlyingCharacter {
     fly(): void {
         console.log("I can fly because I mastered my chi!");
     }
+
+    // override the isAlive method
+    // override est un mot clé purement cosmétique qui permet de signaler qu'on override une méthode parente
+    // si on ne le fait pas, le code compile quand même
+    // mais si on le fait et qu'on ne respecte pas la signature de la méthode parente, le compilateur signale une erreur
+    // c'est donc une bonne pratique de toujours mettre override quand on override une méthode parente
+    override isAlive(): boolean {
+        super.isAlive();
+        return true;
+    }
 }
