837811fdcde224222fff826333a1755d7fe22f8d
BlackB34rd <contact@the-tiny-spark.com>
2025-09-26 14:01:08 +0200

    add factory and singleton management

--- src/heroes/UniqHeroe.ts
+++ src/heroes/UniqHeroe.ts
@@ -1,16 +1,3 @@
 import { Heroe } from "./Heroe";
 
-export class UniqHeroe extends Heroe{
-    private static _instance: UniqHeroe | null = null;
-
-    public static getInstance(): UniqHeroe {
-        if( UniqHeroe._instance == null ){
-            UniqHeroe._instance = new UniqHeroe();
-        }
-        return UniqHeroe._instance;
-    }
-
-    private constructor(){
-        super();
-    }
-}
+export class UniqHeroe extends Heroe{}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import { UniqHeroe } from "./heroes/UniqHeroe";
+import { HeroeFactory, HeroeType } from "./factory/HeroeFactory";
 
-console.log( UniqHeroe.getInstance() === UniqHeroe.getInstance() );
+const factory = new HeroeFactory();
+
+const uniqHeroe1 = factory.createHeroe("Jack Sparrow", 100, HeroeType.SHONEN, true);
+const uniqHeroe2 = factory.createHeroe("Barbossa", 100, HeroeType.SHONEN, true);
+
+console.log( uniqHeroe1 === uniqHeroe2 );  // true
+
+// console.log( factory.createHeroe<IMutant & IFlyingCharacter>("Goku", 150, HeroeType.SHONEN) );
+// console.log( factory.createHeroe<IMutant>("Spiderman", 100, HeroeType.NON_FLYING) );
+// console.log( factory.createHeroe("Jack Sparrow", 100, HeroeType.UNIQ) );
