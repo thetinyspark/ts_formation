86da0242880c191e9c3566e0ef16f31723033688
BlackB34rd <contact@the-tiny-spark.com>
2025-09-26 13:44:58 +0200

    add singleton ( bad pattern )

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { Inventory } from "./ui/Inventory";
-import { Weapon } from "./weapon/Weapon";
+import { UniqHeroe } from "./heroes/UniqHeroe";
 
-const bag = new Inventory<string, Weapon|string>();
-const sword = new Weapon("Sword", 10);
-const axe = new Weapon("Axe", 15);
-
-bag.addItem("excalibur", sword);
-bag.addItem("hache conan", axe);   
-bag.addItem("message", "Tuer les ennemis les voir mourir devant soi");   
-bag.addItem("numEnnemies", "100");   
-
-bag.removeItem("excalibur");
-bag.removeItem("message");
-bag.removeItem("hache conan");
-
-console.log(bag.getIemByKey("numEnnemies"));
+console.log( UniqHeroe.getInstance() === UniqHeroe.getInstance() );
